8e5c9cd25e0e4e2aa7ac77a5c6c4bff24599729e
tmr031 <tmr03133@gmail.com>
2017-08-03 05:12:53 +0900

    Add files via upload

---
+++ client.js
@@ -0,0 +1,69 @@
+/* global TrelloPowerUp */
+var WHITE_ICON = './images/icon-white.svg';
+var GRAY_ICON = './images/icon-gray.svg';
+var Promise = TrelloPowerUp.Promise;
+
+var boardButtonCallback = function(t){
+  return t.popup({
+    title: "Time Statistics",
+    url: './mindmap.html',
+    height: 700
+  });
+};
+
+
+
+var getBadge = function(t){
+	var timerText = '';
+	var color = null;
+	var icon = GRAY_ICON;
+
+	Promise.all([
+		t.get('card', 'private', 'timer_started')
+	])
+	.spread(function(time) {
+		if(time != null) {
+			timerText = 'Timer is on';
+			color = 'red';
+			icon = WHITE_ICON;
+		}
+	});
+
+	return {
+	    dynamic: function(){
+			return {
+				text: timerText,
+				icon: icon,
+				color: color,
+				refresh: 10
+			}
+	    }
+	};
+}
+
+TrelloPowerUp.initialize({
+	'board-buttons': function(t, options) {
+		return [{
+			icon: WHITE_ICON,
+			text: "Time Statistics",
+			callback: boardButtonCallback
+		}];
+	},
+	'card-buttons': function(t, options) {
+		return [{
+			icon: GRAY_ICON,
+			text: "Time Tracker",
+			callback: cardButtonCallback
+		}];
+	},
+	'card-badges': function(t, card) {
+		return getBadge(t);
+	},
+	'show-settings': function(t, options){
+		return t.popup({
+			title: 'Settings',
+			url: './settings.html',
+			height: 184
+		});
+	}
+});
